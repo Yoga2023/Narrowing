9828e9623ec98be7330e155d1d8bea7119a95eea
Yoga2023 <yogakumaravel2023@gmail.com>
2022-04-04 16:28:35 +0530

    narrowing

---
+++ src/com/day2/task/java/Day2TaskDatatypeWithNarrowing.java
@@ -0,0 +1,24 @@
+package com.day2.task.java;
+
+public class Day2TaskDatatypeWithNarrowing {
+public static void main(String[] args) {
+	byte a= 20;
+	System.out.println(a);
+	short b=9596;
+	System.out.println(b);
+	int c = 230896;
+	System.out.println(c);
+	long d= 202396l;
+	System.out.println(d);
+	float e= 20.0295f;
+	System.out.println(e);
+	int f=(int)e;
+	System.out.println(f);
+	double g =2023.9596;
+	System.out.println(g);
+	char h = 'Y';
+	System.out.println(h);
+	boolean i=1995>1996;
+	System.out.println(i);
+}
+}
